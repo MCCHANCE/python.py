3930f644f72985e9b8c8929304bd3918e33044d3
MCCHANCE <unwananyong2021@gmail.com>
2024-02-24 10:57:01 -0800

    first commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+# #prompt the user for their name
+print('hello, world')
+
+name = input('what is your name? ')
+print("Hello ")
+print(name)
+
+
+name = input('what is your name? ')
+print("Hello " + name )
+
+name = input('what is your name? ')
+print("Hello, " , name )
+
+#Ask the user for their name
+name = input("What's your name? ")
+print("hello," , end="")
+print(name)
